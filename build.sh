ef580d6ce08b57bc4aae4dbaa04529fbe843e91c
Alec Glassford <glassford@stanford.edu>
2018-01-03 16:08:31 -0800

    Add updated date

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-echo "$(cat index-top.html)" "$(marked index-middle.md)" "$(cat index-bottom.html)" > public/index.html;
+echo "$(cat index-top.html)" "$(marked index-middle.md)" "<small>Updated $(date +"%a %b %d %Y")</small>" "$(cat index-bottom.html)" > public/index.html;
 firebase serve;
