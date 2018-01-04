e39200ed02699cefaa53d636cf7c08648bc544cc
Alec Glassford <glassford@stanford.edu>
2018-01-03 16:11:09 -0800

    Update favicon

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-echo "$(cat index-top.html)" "$(marked index-middle.md)" "<small>Updated $(date +"%a %b %d %Y")</small>" "$(cat index-bottom.html)" > public/index.html;
+echo "$(cat index-top.html)" "$(marked index-middle.md)" "<small>Updated $(date +"%a %b %d %Y"), Favicon via <a href="https://github.com/twitter/twemoji"> Twemoji</a></small>" "$(cat index-bottom.html)" > public/index.html;
 firebase serve;
