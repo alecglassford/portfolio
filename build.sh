45c857baebe7d844c7107f874a24be666cb1c707
Alec Glassford <glassford@cs.stanford.edu>
2018-07-30 19:21:30 -0700

    Try to streamline build process a bit

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "$(cat index-top.html)" \
-  "<small>Updated $(date +"%a. %b %d, %Y")</small>" \
-  "$(marked index-middle.md)" \
-  "<small>Favicon via <a href="https://github.com/twitter/twemoji">Twemoji</a> (<a href="https://creativecommons.org/licenses/by/4.0/">CC-BY 4.0</a>)</small>" \
-  "$(cat index-bottom.html)" \
-> public/index.html;
